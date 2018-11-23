2460764c8cf4f423b551be88a31f00b81a8e7b83
Calvin Hu <ycalvinhu@gmail.com>
2018-11-23 09:43:17 -0800

    Preparing for deploy

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const path = require('path');
 require('dotenv').config();
 
+// App Setup
 const app = require('./app');
 
-// Deployment Setup (Serve index.html)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+// Deployment Setup
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // Serve index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Server Setup
 const port = process.env.PORT || 5000;
